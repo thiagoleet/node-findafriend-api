4ca612808a709e68fb176f8664774850689d1fc3
Thiago Leite <thiagofmleite@gmail.com>
2024-06-29 16:52:11 -0300

    Added Org Authenticate Use Case

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,2 +1,3 @@
 export * from "./org-already-exists.error";
 export * from "./required-fields.error";
+export * from "./invalid-credentials.error";
--- src/use-cases/authenticate/authenticate.ts
+++ src/use-cases/authenticate/authenticate.ts
@@ -1,1 +1,33 @@
-export class AuthenticateUseCase {}
+import { InvalidCredentialsError } from "@/errors";
+import { OrgRepository } from "@/repositories";
+import { Org } from "@prisma/client";
+import { compare } from "bcryptjs";
+
+type AuthenticateProps = {
+  email: string;
+  password: string;
+};
+
+type AuthenticateResponse = {
+  org: Org;
+};
+
+export class AuthenticateUseCase {
+  constructor(private repository: OrgRepository) {}
+
+  async execute(data: AuthenticateProps): Promise<AuthenticateResponse> {
+    const org = await this.repository.findByEmail(data.email);
+
+    if (!org) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await compare(data.password, org.password_hash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return { org };
+  }
+}
